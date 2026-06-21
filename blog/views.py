18b5ba640182f0b5f04e5f131eb310513cf5ae59
Evghen Sokolov <Evghensokolov@yandex.ru>
2026-06-21 13:46:22 +0300

    force clear unused imports for pep8

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = []
+
+def index(request):
+    return render(request, 'blog/index.html', {'post': posts})
+
+def post_detail(request, id):
+    return render(request, 'blog/detail.html')
+
+def category_posts(request, category_slug):
+    return render(request, 'blog/category.html')
