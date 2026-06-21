027d507c7b1b32c916bb11062d1a9382048b1457
Evghen Sokolov <Evghensokolov@yandex.ru>
2026-06-21 15:11:26 +0300

    auto format views with black

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,12 @@ posts = []
 
 
 def index(request):
-    return render(request, 'blog/index.html', {'post': posts})
+    return render(request, "blog/index.html", {"post": posts})
 
 
 def post_detail(request, id):
-    return render(request, 'blog/detail.html')
+    return render(request, "blog/detail.html")
 
 
 def category_posts(request, category_slug):
-    return render(request, 'blog/category.html')
+    return render(request, "blog/category.html")
