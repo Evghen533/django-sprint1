f72efd7e253b85e2dc52f63d8fb4ce137d63301c
Evghen Sokolov <Evghensokolov@yandex.ru>
2026-06-21 14:41:22 +0300

    manual fix pep8 E302

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 posts = []
 
+
 def index(request):
     return render(request, 'blog/index.html', {'post': posts})
 
+
 def post_detail(request, id):
     return render(request, 'blog/detail.html')
+
 
 def category_posts(request, category_slug):
     return render(request, 'blog/category.html')
