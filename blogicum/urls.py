8933f048aa6cd57b95819cf805feda8b3830350c
Евгений <Evghensokolov@yandex.ru>
2026-07-26 12:57:55 +0300

    Update urls.py

--- blogicum/urls.py
+++ blogicum/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
 ]
+
+# Это сработает ТОЛЬКО если DEBUG = True в settings.py
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
