dc4d4061131c8be348ad1b084991c1059f5fa5da
Evghen Sokolov <Evghensokolov@yandex.ru>
2026-07-31 19:57:33 +0300

    Fix PEP8: E302 and E501 in admin.py and models.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
+
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date', 'author')
     search_fields = ('title', 'text')
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True)
@@ -22,9 +23,23 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     pub_date = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='posts')
-    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='posts',
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        related_name='posts',
+    )
     is_published = models.BooleanField(default=True)
 
     class Meta:
