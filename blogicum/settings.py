3d3ca737dce6fd615eece774eba5c14d137bed2f
Evghen Sokolov <Evghensokolov@yandex.ru>
2026-08-02 20:24:13 +0300

    Cleanup: remove duplicate root blog/ and pages/ folders, fix nested blogicum structure, keep single source of truth in blogicum/

--- blogicum/settings.py
+++ blogicum/settings.py
@@ -24,8 +24,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'pages',
-    "blog",
 ]
 
 MIDDLEWARE = [
--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "pages"  # Переменная app_name обязательна!
-
-urlpatterns = [
-    path("about/", views.about, name="about"),
-    path("rules/", views.rules, name="rules"),
-]
