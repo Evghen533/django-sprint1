0ab189bca15f25465e2b51679571c67ce8100891
Evghen Sokolov <Evghensokolov@yandex.ru>
2026-08-04 21:43:45 +0300

    Fix: align post date format between template and test; ensure ISO date is rendered

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    date = models.CharField(max_length=100)  # если дата у тебя хранится как текст (как в словарях)
+    date = models.DateField()
     location = models.CharField(max_length=200, blank=True)
     category = models.CharField(max_length=50, blank=True)
 
