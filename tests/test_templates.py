e80a6363a84c0005f1dd0a114700f1729d8dbe32
Evghen Sokolov <Evghensokolov@yandex.ru>
2026-08-23 14:19:27 +0300

    move manage.py to project root

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,6 +1,8 @@
+import re                      # <-- добавлено для test_post_list
 import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
+from blog.models import Post, Category  # <-- импортируем модели
 
 @pytest.mark.parametrize(
     'view_name, kwargs, template', [
@@ -15,13 +17,24 @@ from pytest_django.asserts import assertTemplateUsed
 )
 @pytest.mark.django_db
 def test_page_templates(client, view_name, kwargs, template):
+    # Создаём посты прямо здесь, чтобы тест был независимым
+    if view_name == 'blog:post_detail':
+        # Создаём категории, если их нет
+        cat_travel, _ = Category.objects.get_or_create(slug='travel', defaults={'name': 'Путешествия'})
+        cat_adventure, _ = Category.objects.get_or_create(slug='adventure', defaults={'name': 'Приключения'})
+
+        # Очищаем, чтобы не дублировать при повторных запусках (опционально)
+        Post.objects.filter(id__in=[1, 2, 3]).delete()
+
+        Post.objects.create(id=1, title='Post 1', text='Text 1', category=cat_travel, location='Loc 1')
+        Post.objects.create(id=2, title='Post 2', text='Text 2', category=cat_adventure, location='Loc 2')
+        Post.objects.create(id=3, title='Post 3', text='Text 3', category=cat_travel, location='Loc 3')
+
     url = reverse(view_name, kwargs=kwargs)
     response = client.get(url)
     assert response.status_code == 200, f"URL {url} должен возвращать 200, а получил {response.status_code}"
-    assertTemplateUsed(response, template, msg_prefix=(
-        f'Убедитесь, что для отображения страницы `{url}` используется '
-        f'шаблон `{template}`.'
-    ))
+    assertTemplateUsed(response, template, msg_prefix=f'Убедитесь, что для `{url}` используется шаблон `{template}`.')
+
 
 @pytest.mark.django_db
 @pytest.mark.parametrize('post_id', (1, 2, 3))
@@ -33,10 +46,9 @@ def test_post_detail(post_id, client, posts):
     post_obj = response.context.get('post')
     assert post_obj is not None, 'Контекст не содержит ключ "post"'
 
-    expected_post = posts[post_id - 1]  # posts[0] = ID 1 и т.д.
-    assert expected_post == post_obj, (
-        f'На странице {url} должен быть пост с ID {post_id}'
-    )
+    expected_post = posts[post_id - 1]
+    assert expected_post == post_obj, f'На странице {url} должен быть пост с ID {post_id}'
+
 
 @pytest.mark.django_db
 def test_post_list(client, posts):
@@ -51,6 +63,5 @@ def test_post_list(client, posts):
     page_content = response.content.decode('utf-8')
 
     assert re.search(pattern, page_content), (
-        f'Убедитесь, что на странице `{url}` выводится инвертированный список '
-        'постов из задания.'
+        f'Убедитесь, что на странице `{url}` выводится инвертированный список постов.'
     )
